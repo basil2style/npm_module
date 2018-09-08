7702d41832ca9433ef26e57e18a7b2a4e6b1d44b
Basil <basiltalias92@gmail.com>
2018-09-07 22:57:38 -0400

    Added arguments

--- bin/test-module.js
+++ bin/test-module.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
- 
-var lib= require('../lib/index.js');
-var greeting = lib.sayHello('Basil');
- 
-console.log(greeting);
+
+var lib = require('../lib/index.js');
+
+
+var args = process.argv.splice(process.execArgv.length + 2);
+// Retrieve the first argument
+var name = args[0];
+var value = args[1];
+var message = lib.sayHello(name);
+console.log(message + "," + value);
